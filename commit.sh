43acb6c47b5be7802aa86d4e30ccfb818b29d781
Justin Kopepasah <justin@kopepasah.com>
2023-02-08 17:25:45 -0600

    Remove from checkout.

--- commit.sh
+++ commit.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 
-# svn commit \
-#   --username "$1" \
-#   --password "$2" \
-#   --message "$3" \
-#   --no-auth-cache \
-#   --non-interactive
-
-ls -lA
-svn status
-svn auth
+svn commit \
+  --username $1 \
+  --password $2 \
+  --message $3 \
+  --no-auth-cache \
+  --non-interactive
